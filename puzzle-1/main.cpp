5bb267980c7cb0b3cdca66dea7a0be1bd42a3b71
Zachary <z.taylor@hiwin.us>
2023-12-04 22:55:57 -0600

    Adding Current Project

---
+++ puzzle-1/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+char isNumInString(std::string);
+
+int main(){
+    int ans = 0; int count = 0; int test = 0;
+    char first, last, tmpc;
+    std::string temp, num, line;
+    std::ifstream ifs("puzzle-1.txt");
+    
+    while(std::getline(ifs, line)){
+        count = 0;
+        first = ' ';
+        last = ' ';
+        temp.clear();
+        num.clear();
+        for(char& c: line){
+            test = isNumInString(num);
+            if((int(test) - int('0')) > 0){
+                num = num.back();
+                if(count == 0){
+                    first = test;
+                    count++;
+                }
+                last = test;
+                if(isdigit(c)){
+                    last = c;
+                }else num += c;
+            }else if(isdigit(c)){
+                num.clear();
+                if(count == 0){
+                    first = c;
+                    count++;
+                }
+                last = c;
+            }else{
+                num += c;
+            }
+        }
+        test = isNumInString(num);
+        if((int(test) - int('0')) > 0){
+            num.clear();
+            if(count == 0){
+                first = test;
+                count++;
+            }
+            last = test;
+        }
+        temp += first;
+        temp += last;
+        ans += stoi(temp);
+    }
+    ifs.close();
+
+    std::cout << ans << std::endl;
+
+    system("pause");
+    return 0;
+}
+
+char isNumInString(std::string num){
+    if(num.find("one") != std::string::npos) return '1';
+    if(num.find("two") != std::string::npos) return '2';
+    if(num.find("three") != std::string::npos) return'3';
+    if(num.find("four") != std::string::npos) return '4';
+    if(num.find("five") != std::string::npos) return '5';
+    if(num.find("six") != std::string::npos) return '6';
+    if(num.find("seven") != std::string::npos) return '7';
+    if(num.find("eight") != std::string::npos) return '8';
+    if(num.find("nine") != std::string::npos) return '9';
+    return '0';
+}
---
+++ puzzle-2/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#define NUM_RED_CUBES 12
+#define NUM_GREEN_CUBES 13
+#define NUM_BLUE_CUBES 14
+#define GAME_DELIMITER ';'
+#define COLOR_DELIMITER ','
+
+bool try_pair(std::string, std::string);
+
+int main(){
+    int ans = 0; int line_num = 0;
+    std::string line;
+    //temp values for number of cubes in a game
+    int tmpr = 0; int tmpg = 0; int tmpb = 0;
+    bool line_state = 1;
+    std::ifstream file("puzzle-2.txt");
+    std::string num;
+    std::string color;
+
+    while(std::getline(file, line)){
+        line_num++;
+        auto it = line.begin();
+        line_state = 1;
+
+        while(*it != ':'){
+            it++;
+            continue;
+        }
+        it++;
+
+        num.clear(); color.clear();
+        while(it != line.end()){
+            if(*it == COLOR_DELIMITER || *it == GAME_DELIMITER){
+                line_state *= try_pair(color, num);
+                color.clear(); num.clear();
+            }else if(isdigit(*it)){
+                num += *it;
+            }else{
+                color += *it;
+            }
+            it++;
+            if(it == line.end()){
+                line_state *= try_pair(color, num);
+            }
+        }
+
+        if(line_state){
+            ans += line_num;
+            //std::cout << line_num << std::endl;
+        }
+    }
+    std::cout << ans << std::endl;
+    system("pause");
+    return 0;
+}
+
+bool try_pair(std::string color, std::string num){
+    if(color.find("red") != std::string::npos){
+        if(stoi(num) > NUM_RED_CUBES){
+            return false;
+        }
+    }else if(color.find("blue") != std::string::npos){
+        if(stoi(num) > NUM_BLUE_CUBES){
+            return false;
+        }
+    }else if(color.find("green") != std::string::npos){
+        if(stoi(num) > NUM_GREEN_CUBES){
+            return false;
+        }
+    }
+    return true;
+}
