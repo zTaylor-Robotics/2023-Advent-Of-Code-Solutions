e852d8c20054c9c0f1b3ed165c132a4f141f4aea
Zachary <z.taylor@hiwin.us>
2023-12-11 17:08:14 -0600

    completed puzzle-6 part-2

--- puzzle-6/part-2/main.cpp
+++ puzzle-6/part-2/main.cpp
@@ -5,9 +5,8 @@
 #include <map>
 #include <time.h>
 
-void readFile(std::vector<std::vector<int>> &, std::ifstream &);
-int getNumWins(int, int);
-bool isWinner(int, int, int);
+void readFile(long long &, long long &, std::ifstream &);
+long long getNumWins(long long, long long);
 
 int main(){
     std::clock_t t = std::clock();
@@ -15,28 +14,20 @@ int main(){
     std::string file_name = "./puzzle-6.txt";
     std::ifstream file(file_name);
 
-    std::vector<std::vector<int>> data; //vector where each row reprents [time][record]
-    int output = 1; //logs the multiple of the number of wins per data point
+    long long time = 0; long long record = 0;
 
-    readFile(data, file); //grab the times and records from the input file
-
-    //Grab the number of wins per point and multiply them by the previous wins.
-    for(auto& point: data){
-        output *= getNumWins(point[0], point[1]);
-        std::cout << getNumWins(point[0], point[1]) << std::endl;
-    }
+    readFile(time, record, file); //grab the times and records from the input file
 
     //system("pause");
-    std::cout << "The result of multiplying the number of ways to win each race: " << output << std::endl;
+    std::cout << "The result number of ways to win the race: " << getNumWins(time, record) << std::endl;
     std::cout << "Total Execution time: " <<  (std::clock() - t ) / (float)CLOCKS_PER_SEC << " seconds" << std::endl;
     return 0;
 }
 
-void readFile(std::vector<std::vector<int>> &data, std::ifstream &file){
+void readFile(long long &time, long long &record, std::ifstream &file){
     std::string line;
-    std::vector<int> temp;
-    bool num_start = 0;
-    bool resize_complete = 0;
+    std::string int_value;
+    bool time_flag = true;
 
     while(std::getline(file, line)){
         auto it = line.begin();
@@ -45,51 +36,31 @@ void readFile(std::vector<std::vector<int>> &data, std::ifstream &file){
 
         while(it != line.end()){
             if(isdigit(*it)){
-                if(!num_start){
-                    int_start = it;
-                    num_start = true;
-                }
-            }else{
-                if(num_start){
-                    int_end = it;
-
-                    if(!resize_complete){
-                        data.push_back(std::vector<int>(2, 0));
-                    }
-
-                    temp.push_back(std::stoi(std::string(int_start, int_end)));
-                    num_start = false;
-                }
+                int_value = int_value + *it;
             }
             it++;
         }
-        if(num_start){
-            int_end = it;
-
-            if(!resize_complete){
-                data.push_back(std::vector<int>(2, 0));
-            }
-
-            temp.push_back(std::stoi(std::string(int_start, int_end)));
-            num_start = false;
+        
+        if(time_flag){
+            std::cout << int_value << std::endl;
+            time = std::stoll(int_value);
+            time_flag = false;
+            int_value.clear();
+        }else{
+            std::cout << int_value << std::endl;
+            record = std::stoll(int_value);
+            return;
         }
-
-        for(int i = 0; i < temp.size(); i++){
-            data[i][resize_complete] = temp[i];
-        }
-
-        temp.clear();
-        resize_complete = true;
     }
     return;
 }
 
 
-int getNumWins(int time, int record){
-    int ret = 0;
+long long getNumWins(long long time, long long record){
+    long long ret = 0;
 
-    int right_zero = 0.5*time + 0.5*std::sqrt(std::pow(time, 2) - 4*record - 4);
-    int left_zero  = 0.5*time - 0.5*std::sqrt(std::pow(time, 2) - 4*record - 4) + 1;
+    long long right_zero = 0.5*time + 0.5*std::sqrt(std::pow(time, 2) - 4*record - 4);
+    long long left_zero  = 0.5*time - 0.5*std::sqrt(std::pow(time, 2) - 4*record - 4) + 1;
 
     ret = right_zero - left_zero + 1;
 
