6e24251ad4b3a4141348314d2af57d55a0c50344
Zachary <z.taylor@hiwin.us>
2023-12-13 16:31:54 -0600

    updating with puzzle-8

--- puzzle-8/part-1/main.cpp
+++ puzzle-8/part-1/main.cpp
@@ -5,13 +5,37 @@
 #include <map>
 #include <time.h>
 
+void getTurnList(std::vector<bool> &, std::ifstream &);
+void getNumSteps();
+
 int main(){
     std::clock_t t = std::clock();
 
+    //stores the file to be pointed to later
     std::string file_name = "./puzzle-8.txt";
     std::ifstream file(file_name);
-    
+
+    //get the list of turns made
+    std::vector<bool> turn_list;
+    getTurnList(turn_list, file); //Fetches the turns from the file
+
+
+
+    std::cout << std::endl;
     //system("pause");
     std::cout << "Total Execution time: " <<  (std::clock() - t ) / (float)CLOCKS_PER_SEC << " seconds" << std::endl;
     return 0;
+}
+
+void getTurnList(std::vector<bool> &list, std::ifstream &file){
+    std::string line;
+    std::getline(file, line);
+
+    for(char& c: line){
+        if(c == 'L') list.push_back(false);
+        else if(c == 'R') list.push_back(true);
+    }
+
+    std::getline(file, line); //get the blank line underneath the turn list
+    return;
 }
