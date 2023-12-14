fe2de261b55f787d5a0781321cd7cfd9019699ea
Zachary <z.taylor@hiwin.us>
2023-12-14 16:09:49 -0600

    Finished Part 1 of Puzzle 8

--- puzzle-8/part-1/main.cpp
+++ puzzle-8/part-1/main.cpp
@@ -6,10 +6,13 @@
 #include <time.h>
 
 void getTurnList(std::vector<bool> &, std::ifstream &);
-void getNumSteps();
+void getMap(std::map<std::string, std::pair<std::string, std::string>> &, std::ifstream &);
+int getNumSteps(std::map<std::string, std::pair<std::string, std::string>> &, std::vector<bool> &);
+
+void printMap(std::map<std::string, std::pair<std::string, std::string>> &);
 
 int main(){
-    std::clock_t t = std::clock();
+    std::clock_t tot = std::clock();
 
     //stores the file to be pointed to later
     std::string file_name = "./puzzle-8.txt";
@@ -19,11 +22,20 @@ int main(){
     std::vector<bool> turn_list;
     getTurnList(turn_list, file); //Fetches the turns from the file
 
+    //create map containing all the steps as [key][value] pairs where the key is the
+    //milestone and the value is a pair(left milestone,right milestone)
+    std::map<std::string, std::pair<std::string, std::string>> ms_map;
+    getMap(ms_map, file);
 
 
-    std::cout << std::endl;
+    //this is a very bruteforce method for getting the number of steps
+    //primarily because of the puzzle context
+    int numsteps = getNumSteps(ms_map, turn_list);
+
+    std::cout << "Number of Steps: " << numsteps << std::endl;
+
     //system("pause");
-    std::cout << "Total Execution time: " <<  (std::clock() - t ) / (float)CLOCKS_PER_SEC << " seconds" << std::endl;
+    std::cout << "Total Execution time: " <<  (std::clock() - tot ) / (float)CLOCKS_PER_SEC << " seconds" << std::endl;
     return 0;
 }
 
@@ -39,3 +51,40 @@ void getTurnList(std::vector<bool> &list, std::ifstream &file){
     std::getline(file, line); //get the blank line underneath the turn list
     return;
 }
+
+void getMap(std::map<std::string, std::pair<std::string, std::string>> &map, std::ifstream &file){
+    std::string line, key, left, right;
+
+    while(std::getline(file, line)){
+        key = line.substr(0, 3);
+        left = line.substr(7, 3);
+        right = line.substr(12, 3);
+        map[key] = std::pair<std::string, std::string> (left, right);
+    }
+
+    return;
+}
+
+int getNumSteps(std::map<std::string, std::pair<std::string, std::string>> &map, std::vector<bool> &step){
+    std::string current_milestone = "AAA";
+    int count = 0; int vect_it = 0;
+
+    while(current_milestone != "ZZZ"){
+        if(vect_it == step.size()) vect_it = 0;
+
+        if(step[vect_it]) current_milestone = map[current_milestone].second;
+        else current_milestone = map[current_milestone].first;
+        count++;
+        vect_it++;
+    }
+
+    return count;
+}
+
+void printMap(std::map<std::string, std::pair<std::string, std::string>> &map){
+    for(auto& row: map){
+        std::cout << row.first << " = (" << row.second.first << ", " << row.second.second << ")" << std::endl;
+    }
+    return;
+}
+
