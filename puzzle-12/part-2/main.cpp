afe1b2acc02061a28db5debf7817ca4262b3a04b
SH3P <sh3p.productions@gmail.com>
2024-01-15 00:12:58 -0600

    update problem 12

--- puzzle-12/part-2/main.cpp
+++ puzzle-12/part-2/main.cpp
@@ -1,18 +1,21 @@
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <mutex>
 #include <string>
 #include <vector>
+#include <map>
 #include <ctime>
-#include <cmath>
 
 unsigned long readFile(std::ifstream &);
-unsigned long long getNum(std::vector<char> &, std::vector<unsigned long> &); //returns the number of good combinations of springs
-void countGoodConfigs(std::vector<char> &, std::vector<unsigned long> &, std::vector<unsigned long> &, unsigned long &, unsigned long &, unsigned long);
 
-std::vector<char> buildConfig(std::vector<unsigned long>, std::vector<unsigned long> &);
-std::vector<char> buildTest(std::vector<unsigned long>, std::vector<unsigned long> &, unsigned long);
-bool cmpConfig(std::vector<char> &, std::vector<char> &);
+unsigned long long getNum(std::vector<char> &, 
+                          std::vector<unsigned long> &); //returns the number of good combinations of springsss
 
+unsigned long long recurse(std::vector<char> &, 
+                           std::vector<unsigned long> &, 
+                           std::map<std::pair<unsigned long, std::vector<char>>, unsigned long long> &,
+                           unsigned long);
 
 int main(){
     std::ios_base::sync_with_stdio(false);
@@ -36,6 +39,7 @@ unsigned long readFile(std::ifstream &file){
     std::vector<unsigned long> groups;
     unsigned long long count = 0;
     bool swap = true;
+
     while(std::getline(file, line)){
         springs.clear(); groups.clear(); temp.clear(); swap = true;
         for(const char c: line){
@@ -59,102 +63,27 @@ unsigned long readFile(std::ifstream &file){
 }
 
 //generate possible strings and compare each string with the base strings: springs
-unsigned long long getNum(std::vector<char> &springs, std::vector<unsigned long> &groups){
-    unsigned long count1 = 0; //temp, change this
-    unsigned long count2 = 0;
+unsigned long long getNum(std::vector<char> &springs, 
+                          std::vector<unsigned long> &groups){
 
-    std::vector<unsigned long> values(groups.size() + 1, 0); //initialize the empty value vector to kick off recursion
-    unsigned long R = springs.size();
+    unsigned long long count = 0;
+    std::map<std::pair<unsigned long, std::vector<char>>, unsigned long long> memo;
+    unsigned long depth = 0;
+    count = recurse(springs, groups, memo, depth);
 
-    for(unsigned long g: groups){
-        R -= g;
-    }
-    R -= groups.size() - 1;
-    values[0] = R;
-
-    countGoodConfigs(springs, values, groups, count1, R, 0);
-
-    values.clear();
-
-    unsigned long length = groups.size();
-    for(unsigned long i = 0; i < length; i++) groups.push_back(groups[i]);
-
-    values = std::vector<unsigned long>(groups.size() + 1, 0);
-    
-    length = springs.size();
-    springs.push_back('?');
-    for(unsigned long j = 0; j < length; j++) springs.push_back(springs[j]);
-    
-    R = springs.size();
-    
-    for(unsigned long g: groups){
-        R -= g;
-    }
-    R -= groups.size() - 1;
-    values[0] = R;
-
-    countGoodConfigs(springs, values, groups, count2, R, 0);
-    
-    return (count2*count2*count2*count2)/(count1*count1*count1);
+    return count;
 }
 
-void countGoodConfigs(std::vector<char> &springs, std::vector<unsigned long> &vals, std::vector<unsigned long> &groups, unsigned long &count, unsigned long &R, unsigned long depth){
-    if(depth >= (vals.size() - 1)){
-        std::vector<char> temp = buildConfig(vals, groups);
-        if(cmpConfig(springs, temp)){
-            count++;
-        }
-        return;
+unsigned long long recurse(std::vector<char> &springs, 
+                           std::vector<unsigned long> &groups, 
+                           std::map<std::pair<unsigned long, std::vector<char>>, unsigned long long> &memo,
+                           unsigned long depth){
+    unsigned long long count = 0;
+
+    if(depth == (groups.size() - 1)){
+      return 1;
     }
 
-    for(unsigned long i = 0; i <= vals[depth]; i++){
-        std::vector<unsigned long> temp = vals;
-        temp[depth] -= i;
-        temp[depth+1] = R;
-        for(unsigned long j = 0; j <= depth; j++){
-            temp[depth+1] -= temp[j];
-        }
-
-        std::vector<char> test = buildTest(temp, groups, depth);
-        if(cmpConfig(springs, test)){
-            countGoodConfigs(springs, temp, groups, count, R, depth + 1);
-        }
-    }
-
-    return;
-}
-
-bool cmpConfig(std::vector<char> &springs, std::vector<char> &config){
-    for(unsigned long i = 0; i < config.size(); i++){
-        if(springs[i] == '.' && config[i] == '#'){
-            return false;
-        }else if(springs[i] == '#' && config[i] == '.'){
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<char> buildConfig(std::vector<unsigned long> vals, std::vector<unsigned long> &groups){
-    std::vector<char> config;
-
-    for(unsigned long i = 1; i < vals.size() - 1; i++) vals[i]++; //add in the necessary . between groups of springs
-
-    for(unsigned long i = 0; i < vals.size(); i++){
-        for(unsigned long j = 0; j < vals[i]; j++){config.push_back('.');}
-        if(i < (vals.size() - 1)){for(unsigned long k = 0; k < groups[i]; k++){config.push_back('#');}}
-    }
-    return config;
-}
-
-std::vector<char> buildTest(std::vector<unsigned long> vals, std::vector<unsigned long> &groups, unsigned long depth){
-    std::vector<char> test;
-
-    for(unsigned long i = 1; i < vals.size() - 1; i++) vals[i]++; //add in the necessary . between groups of springs
-        
-    for(unsigned long i = 0; i <= depth; i++){
-        for(unsigned long j = 0; j < vals[i]; j ++) test.push_back('.');
-        for(unsigned long k = 0; k < groups[i]; k ++) test.push_back('#');
-    }
-    return test;
+    count += recurse(springs, groups, memo, depth + 1);
+    return count;
 }
